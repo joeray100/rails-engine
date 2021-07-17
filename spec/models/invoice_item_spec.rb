987cdd4d2031a366bb092732fcbf009b2c9b9c58
Joe Ray <jray100.jr@gmail.com>
2021-07-17 15:49:03 -0600

    Update model tests with relationships

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:item) }
+    it { should belong_to(:invoice) }
+  end
+
+  # describe 'validations' do
+  #   it { should validate_presence_of(:) }
+  # end
+  #
+  # before :each do
+  #
+  # end
+  #
+  # describe 'class methods' do
+  #   describe '.' do
+  #   end
+  # end
+  #
+  # describe 'instance methods' do
+  #   describe '#' do
+  #   end
+  # end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:merchant) }
+    it { should have_many(:invoice_items) }
+  end
+
+  # describe 'validations' do
+  #   it { should validate_presence_of(:) }
+  # end
+  #
+  # before :each do
+  #
+  # end
+  #
+  # describe 'class methods' do
+  #   describe '.' do
+  #   end
+  # end
+  #
+  # describe 'instance methods' do
+  #   describe '#' do
+  #   end
+  # end
 end
