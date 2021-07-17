17e489a60b4024163bad5b70e861e2f44b6ff9d5
Joe Ray <jray100.jr@gmail.com>
2021-07-17 15:53:08 -0600

    Made alterations to merchants index spec

--- spec/requests/api/v1/merchants/index_spec.rb
+++ spec/requests/api/v1/merchants/index_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Merchants API' do
   before :each do
-    create_list(:merchant, 80)
+    FactoryBot.reload
   end
 
   describe 'happy path' do
     it 'sends a list of no more than 20 merchants' do
+      create_list(:merchant, 80)
+
       get '/api/v1/merchants'
       expect(response).to be_successful
       merchants = JSON.parse(response.body, symbolize_names: true)[:data]
@@ -28,18 +30,19 @@ RSpec.describe 'Merchants API' do
     end
 
       it 'sends the correct amount of merchants based on the page value' do
+        create_list(:merchant, 80)
+
         get '/api/v1/merchants?page=1'
         expect(response).to be_successful
         merchants = JSON.parse(response.body, symbolize_names: true)[:data]
         expect(merchants.count).to eq(20)
-
-        get '/api/v1/merchants?page=2'
-        expect(response).to be_successful
-        merchants = JSON.parse(response.body, symbolize_names: true)[:data]
-        expect(merchants.count).to eq(20)
+        require 'pry'; binding.pry
+        expect(merchants.first[:id]).to eq(1)
       end
 
       it 'sends the correct amount of merchants based on the per page value' do
+        create_list(:merchant, 80)
+
         get '/api/v1/merchants?per_page=13'
         expect(response).to be_successful
         merchants = JSON.parse(response.body, symbolize_names: true)[:data]
@@ -47,6 +50,8 @@ RSpec.describe 'Merchants API' do
       end
 
       it 'sends the correct amount of merchants based on the per page & page value' do
+        create_list(:merchant, 80)
+
         get '/api/v1/merchants?per_page=18page=1'
         expect(response).to be_successful
         merchants = JSON.parse(response.body, symbolize_names: true)[:data]
@@ -54,10 +59,17 @@ RSpec.describe 'Merchants API' do
       end
     end
 
-  # describe 'sad path' do
-  #   it '' do
-  #     get '/api/v1/'
-  #
-  #   end
-  # end
+  describe 'sad path' do
+    it 'returns an empty array of merchants when called correctly' do
+      get '/api/v1/merchants'
+
+      expect(response).to be_successful
+      merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(merchants.count).to eq(0)
+
+
+      expect(merchants).to be_a(Array)
+      expect(merchants).to be_empty
+    end
+  end
 end
