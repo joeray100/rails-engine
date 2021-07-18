816623304b5554c1b1cabc5acb7e255f0b0588fa
Joe Ray <jray100.jr@gmail.com>
2021-07-17 20:12:21 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: :index
+
+      resources :merchants, only: [:index, :show] do
+        scope module: 'merchant_items' do
+          resources :items, only: :index
+        end
+      end
+      
     end
   end
 end
