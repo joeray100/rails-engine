d16881851ec44f28cce8c7aca88328eb19f3a1af
Joe Ray <jray100.jr@gmail.com>
2021-07-17 20:16:56 -0600

    Update merchants controller w/ show action

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,3 +3,9 @@ class Api::V1::MerchantsController < ApplicationController
     merchants = Merchant.paging(params.fetch(:per_page, 20).to_i, params.fetch(:page, 1).to_i)
     render json: MerchantSerializer.new(merchants)
   end
+
+  def show
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant)
+  end
+end
