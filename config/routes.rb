ebab2667d2766bb45528370fc7877a1dbdb03bb7
Joe Ray <jray100.jr@gmail.com>
2021-07-18 23:31:00 -0600

    Update routes, items factory, & add methods to app record

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,28 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
+      namespace :merchants do
+        get '/find_all', to: 'search#index'
+      end
+
+      namespace :items do
+        get '/find', to: 'search#show'
+      end
+
       resources :merchants, only: [:index, :show] do
         scope module: 'merchant_items' do
           resources :items, only: :index
         end
       end
-      
+
+      resources :items do
+        scope module: 'items_merchant' do
+          resources :merchant, only: :index
+        end
+      end
+
+      resources :items
+
     end
   end
 end
