bc1b2d258b82ebe9bfe092dc5dd907ffc3978c68
Joe Ray <jray100.jr@gmail.com>
2021-07-21 22:25:29 -0600

    Update item model for min/max price on item search

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,24 @@ class Item < ApplicationRecord
   has_many :invoice_items
 
   validates_presence_of :name, :description, :unit_price, :merchant_id
+
+  class << self
+    def min_price(price)
+      where('unit_price >= ?', "#{price}").
+      order(:name).
+      first
+    end
+
+    def max_price(price)
+      where('unit_price <= ?', "#{price}").
+      order(:name).
+      first
+    end
+
+    def min_and_max_price(min, max)
+      where('unit_price between ? and ?', "#{min}", "#{max}").
+      order(:name).
+      first
+    end
+  end
 end
