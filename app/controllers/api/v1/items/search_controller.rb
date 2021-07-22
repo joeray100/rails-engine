052d84250819aa19a0606b2e05827cccb20b9001
Joe Ray <jray100.jr@gmail.com>
2021-07-21 22:26:31 -0600

    Update show action for min/max params, delete commented code

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::Items::SearchController < ApplicationController
   def show
-    if params[:name]
-      items = Item.find_item(params[:name]).first
-      render json: ItemSerializer.new(items)
+    item = if params[:name]
+      Item.find_item(params[:name])
+    elsif params[:min_price]
+      Item.min_price(params[:min_price])
+    elsif params[:max_price]
+      Item.max_price(params[:max_price])
+    elsif params[:min_price] && params[:max_price]
+      Item.min_and_max_price(params[:min_price], params[:max_price])
     end
+    render json: ItemSerializer.new(item)
   end
 end
