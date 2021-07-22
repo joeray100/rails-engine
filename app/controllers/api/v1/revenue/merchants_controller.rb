c66cf1ef96e699f3f91b89c09530674923ab20d1
Joe Ray <jray100.jr@gmail.com>
2021-07-21 21:24:50 -0600

    Add index action to Rev Merchants controller

--- app/controllers/api/v1/revenue/merchants_controller.rb
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::Revenue::MerchantsController < ApplicationController
+  def index
+    merchants = Merchant.by_revenue(params[:quantity] || 5)
+    render json: MerchantNameRevenueSerializer.new(merchants)
+  end
+
   def show
     merchant = Merchant.find(params[:id])
     render json: MerchantRevenueSerializer.new(merchant)
